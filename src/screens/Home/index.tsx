255beb0b9ff07eebdbdf7c78c712c5d0047edfed
Allan <allangeek@gmail.com>
2022-01-10 14:34:27 -0300

    Cria o componente card car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
+import { Car } from '../../components/Car';
 import Logo from '../../assets/logo.svg';
 
 import { Container, Header, TotalCars, HeaderContent } from './styles';
 
 export function Home() {
+  const carDataOne = {
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  };
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail:
+      'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -21,6 +43,9 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
