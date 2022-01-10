18e3295c3daf6459d0d469b2bd79012c55484712
Allan <allangeek@gmail.com>
2022-01-10 12:04:42 -0300

    Adiciona o tema global do app

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  useFonts, // load fonts
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+import AppLoading from 'expo-app-loading'; // hold a splash screen
+import { ThemeProvider } from 'styled-components'; // create a context with our theme in order to deliver it to the entire App
+
+import { Home } from './src/screens/Home';
+import theme from './src/styles/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  // holds the splash screen until all fonts are loaded
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
