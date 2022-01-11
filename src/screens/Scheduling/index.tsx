bc2cfdda11509efba1b1f8507650f02fd33a0cd1
Allan <allangeek@gmail.com>
2022-01-11 12:24:57 -0300

    Cria a tela de aluguel de carros

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+import { StatusBar } from 'react-native';
+import { Button } from '../../components/Button';
+
+export function Scheduling() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        <BackButton color={theme.colors.shape} onPress={() => {}} />
+
+        <Title>
+          Escolha uma data{'\n'}
+          de início e{'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title='Confirmar' />
+      </Footer>
+    </Container>
+  );
+}
