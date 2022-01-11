0c740533744526ba98537d2d6c92aaded806f559
Allan <allangeek@gmail.com>
2022-01-11 13:16:50 -0300

    Adiciona o calendario na tela de aluguel

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -16,6 +16,7 @@ import {
 } from './styles';
 import { StatusBar } from 'react-native';
 import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 
 export function Scheduling() {
   const theme = useTheme();
@@ -53,7 +54,9 @@ export function Scheduling() {
         </RentalPeriod>
       </Header>
 
-      <Content></Content>
+      <Content>
+        <Calendar />
+      </Content>
 
       <Footer>
         <Button title='Confirmar' />
