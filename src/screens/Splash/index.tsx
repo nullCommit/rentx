dc03cbf610f47782843684da8ba532660a6641b2
Allan <allangeek@gmail.com>
2022-01-16 00:32:39 -0300

    Cria a tela de splash ja com logo

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Button, StyleSheet, Dimensions } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
 
 import { Container } from './styles';
 
+const WIDTH = Dimensions.get('window').width;
+
 export function Splash() {
+  const animation = useSharedValue(0);
+
+  const animetedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 2000,
+            easing: Easing.bezier(0, 0.84, 0.41, 1.04),
+          }),
+        },
+      ],
+    };
+  });
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * (WIDTH - 100);
+  }
+
   return (
     <Container>
-      <Container></Container>
+      <Animated.View style={[styles.box, animetedStyle]} />
+      <Button title='Mover' onPress={handleAnimationPosition} />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+
+    backgroundColor: 'red',
+  },
+});
