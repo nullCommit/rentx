fb506fec2f6aa71cec38af7065a494ccac0d8f19
Allan <allangeek@gmail.com>
2022-01-19 08:30:36 -0300

    Seta o type do icon para o input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,8 +3,9 @@ import { StatusBar } from 'react-native';
 import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
 
-import { Container, Header, Title, Subtitle, Footer } from './styles';
+import { Container, Header, Title, Subtitle, Form, Footer } from './styles';
 
 export function SignIn() {
   const theme = useTheme();
@@ -27,6 +28,10 @@ export function SignIn() {
           uma experiência incrível.
         </Subtitle>
       </Header>
+
+      <Form>
+        <Input iconName='mail' />
+      </Form>
 
       <Footer>
         <Button
