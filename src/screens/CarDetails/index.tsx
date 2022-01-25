3f22910988946dbc720ae6655794963dfaad89fc
Allan <allangeek@gmail.com>
2022-01-25 17:26:27 -0300

    Refactor da tipagem de carsDTO

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -119,8 +119,8 @@ export function CarDetails() {
           </Description>
 
           <Rent>
-            <Period>{car.rent.period}</Period>
-            <Price>R$ {car.rent.price}</Price>
+            <Period>{car.period}</Period>
+            <Price>R$ {car.price}</Price>
           </Rent>
         </Details>
 
@@ -133,13 +133,7 @@ export function CarDetails() {
             />
           ))}
         </Accessories>
-        <About>
-          {car.about}
-          {car.about}
-          {car.about}
-          {car.about}
-          {car.about}
-        </About>
+        <About>{car.about}</About>
       </Animated.ScrollView>
 
       <Footer>
