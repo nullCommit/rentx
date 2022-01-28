0fd2bc17cc51a20057151b586fd2a580a9aba38e
Allan <allangeek@gmail.com>
2022-01-28 12:54:11 -0300

    Cria a tela de perfil de usuario

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import { Feather } from '@expo/vector-icons';
+
+import { BackButton } from '../../components/BackButton';
+
+import {
+  Container,
+  Header,
+  HeaderTop,
+  HeaderTitle,
+  LogoutButton,
+  PhotoContainer,
+  Photo,
+  PhotoButton,
+} from './styles';
+
+export function Profile() {
+  const navigation = useNavigation<any>();
+
+  const theme = useTheme();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  function handleSignOut() {}
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTop>
+          <BackButton color={theme.colors.shape} onPress={handleBack} />
+
+          <HeaderTitle>Editar Perfil</HeaderTitle>
+
+          <LogoutButton onPress={handleSignOut}>
+            <Feather name='power' size={24} color={theme.colors.shape} />
+          </LogoutButton>
+        </HeaderTop>
+
+        <PhotoContainer>
+          <Photo
+            source={{
+              uri: 'https://avatars.githubusercontent.com/u/17691420?v=4',
+            }}
+          />
+
+          <PhotoButton onPress={() => {}}>
+            <Feather name='camera' size={24} color={theme.colors.shape} />
+          </PhotoButton>
+        </PhotoContainer>
+      </Header>
+    </Container>
+  );
+}
